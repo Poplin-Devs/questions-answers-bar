11d3aa8310fc4f0234fa4a1efd7fead9179aaa8d
brynrmrzz <brynrmrz@outlook.com>
2022-01-13 12:24:18 -0500

    Scripts- parse csv for incomplete entries

---
+++ scripts/answers-parse.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+INPUT=answers.csv
+OUTPUT=answers_cleaned.csv
+LOG=answers_error_log.csv
+
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read id question_id body date answerer_name answerer_email reported helpful
+
+BODYLENGTH=${#body}
+NAMELENGTH=${#answerer_name}
+EMAILLENGTH=${#answerer_email}
+do
+
+  if [[ reported -eq 1 ]]
+    then reported=true
+  else
+    reported=false
+  fi
+
+  if [[ -n id && -n question_id ]] &&
+  [[ -n answerer_name && -n answerer_email && -n reported ]] &&
+  [[ BODYLENGTH -lt 1000 && NAMELENGTH -lt 60 && EMAILLENGTH -lt 60 ]]
+  then
+  #Probably should have a INSERT query here
+    echo "$id, $question_id, $body, $date, $answerer_name, $answerer_email, $reported, $helpful" >> $OUTPUT
+  else
+    echo "Invalid entry: $id, $question_id, $body, $date, $answerer_name, $answerer_email, $reported, $helpful" >> $LOG
+  fi
+
+done < $INPUT
+IFS=$OLDIFS
